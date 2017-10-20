16c874c433dd0d65a99fadeccfeb2b426535bbf5
elbenfreund <elbenfreund@DenkenInEchtzeit.net>
2017-10-20 15:06:01 +0200

    Re-add generic main template dir to path

--- dezentrale_web/config/settings/common.py
+++ dezentrale_web/config/settings/common.py
@@ -135,6 +135,7 @@ class Common(Configuration):
     TEMPLATES = [
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [os.path.join(BaseDir.BASE_DIR, 'templates'), ],
             'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
