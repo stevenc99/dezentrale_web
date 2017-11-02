b7f090d5b6f31ecc93029f50310ad8dbbc5a4e85
hackopedia31 <wladi.schneider13@googlemail.com>
2017-11-02 21:20:18 +0100

    add infrastructure page

--- dezentrale_web/config/urls.py
+++ dezentrale_web/config/urls.py
@@ -13,6 +13,7 @@ from wagtail.wagtaildocs import urls as wagtaildocs_urls
 urlpatterns = [
     url(r'^$', generic_views.TemplateView.as_view(template_name='landing_page.html'), name='landing_page'),
     url(r'^impressum/', generic_views.TemplateView.as_view(template_name='impressum.html'), name='impressum'),
+    url(r'^infrastruktur/', generic_views.TemplateView.as_view(template_name='infrastruktur.html'), name='infrastruktur'),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^django-admin/', include(admin.site.urls)),
     # Wagtail
