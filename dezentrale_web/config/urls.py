fe1462278e911b19f5b492cb55366ea9f47c1cc0
hackopedia31 <wladi.schneider13@googlemail.com>
2017-12-07 20:25:21 +0100

    solve flake8 CI error

--- dezentrale_web/config/urls.py
+++ dezentrale_web/config/urls.py
@@ -9,11 +9,16 @@ from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 urlpatterns = [
-    url(r'^$', generic_views.TemplateView.as_view(template_name='landing_page.html'), name='landing_page'),
-    url(r'^impressum/', generic_views.TemplateView.as_view(template_name='impressum.html'), name='impressum'),
-    url(r'^infrastruktur/', generic_views.TemplateView.as_view(template_name='infrastruktur.html'), name='infrastruktur'),
-    url(r'^dokumente/', generic_views.TemplateView.as_view(template_name='dokumente.html'), name='dokumente'),
-    url(r'^girokonto/', generic_views.TemplateView.as_view(template_name='giro.html'), name='giro'),
+    url(r'^$', generic_views.TemplateView.as_view(template_name='landing_page.html'),
+        name='landing_page'),
+    url(r'^impressum/', generic_views.TemplateView.as_view(template_name='impressum.html'),
+        name='impressum'),
+    url(r'^infrastruktur/', generic_views.TemplateView.as_view(
+        template_name='infrastruktur.html'), name='infrastruktur'),
+    url(r'^dokumente/', generic_views.TemplateView.as_view(template_name='dokumente.html'),
+        name='dokumente'),
+    url(r'^girokonto/', generic_views.TemplateView.as_view(template_name='giro.html'),
+        name='giro'),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^django-admin/', include(admin.site.urls)),
     # Wagtail
